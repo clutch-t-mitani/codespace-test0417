2540821f8b3ec00adaca3030155e755de4a54cb3
clutch-t-mitani <85106049+clutch-t-mitani@users.noreply.github.com>
2023-04-21 00:44:40 +0000

    delete unnecessary  file

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +16,3 @@ use App\Http\Controllers\TestController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test', [TestController::class, 'index'])->name('index');
